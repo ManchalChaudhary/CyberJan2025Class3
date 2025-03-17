3c0d6de4b0e21c95bb0510abaddca59bc68daf3a
ManchalChaudhary <95185678+ManchalChaudhary@users.noreply.github.com>
2025-03-17 17:29:15 -0500

    Update StringSplit.java

--- class3/src/stringFunctions/StringSplit.java
+++ class3/src/stringFunctions/StringSplit.java
@@ -3,7 +3,7 @@ package stringFunctions;
 public class StringSplit {
 
 	public static void main(String[] args) {
-		String fruits = "What is you name ?";
+		String fruits = "Hello ! What is you name ?";
 		String[] fruitArray = fruits.split(" ");
 		System.out.println(fruitArray[1]);
 		System.out.println(fruitArray.length-1);
